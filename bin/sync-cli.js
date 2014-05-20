bf7170a0ed937d6e6063507f6fc76be59376afab
edmoz <edwong@mozilla.com>
2014-05-20 15:51:12 -0700

    add perf monitoring

--- bin/sync-cli.js
+++ bin/sync-cli.js
@@ -2,6 +2,7 @@
 
 var dataTypes = ["bookmarks","history","passwords","tabs","addons","prefs","forms"];
 var envTypes = ["prod", "stage"];
+var FIREBASE_URL = "https://fxa-sync.firebaseio.com";
 
 var prod = {
   syncAuthUrl: 'https://token.services.mozilla.com',
@@ -31,6 +32,7 @@ var argv = require('optimist')
   .describe('command', 'action to perform {fetch|push}')
   .default("env", "prod")
   .describe('env', "target environment: stage/prod or the name of an ephemeral")
+  .describe('perf', "send performance data to firebase")
   .describe('quiet', "hide sync data print");
 
 var args = argv.argv;
@@ -45,16 +47,47 @@ if (args['env'] === "stage"){
 }
 console.log("env:\n"+JSON.stringify(options));
 
+var request = require('request');
 var FxSync = require('fx-sync');
 
 var sync = new FxSync({ email: args['email'], password: args['password'] }, options);
 
+function sendPerfData(startTime, total_time){
+  console.log('sync-cli-total: ' + total_time );
+  var epoch = Math.round(startTime/1000);
+  var perfDb = "test";
+  if (args['perf'] !== true){
+    perfDb = args['perf'];
+  }
+  var options = {
+    uri: "https://fxa-sync.firebaseio.com/"+perfDb +"/"+ epoch +".json",
+    method: 'PUT',
+    json: {
+      "totalTime": total_time
+    }
+  };
+
+  request(
+    options,
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        }
+    }
+  );
+}
+
 function fetchData(type){
+  var startTime = (new Date).getTime();
   sync.fetch(type)
           .then(function(data) {
+            var totalTime = ((new Date).getTime() - startTime);
             if(!args['quiet']){
               console.log("::"+type+"::");
               console.log(JSON.stringify(data, null, 2));
+            }
+            if (args['perf']){
+              sendPerfData(startTime, totalTime);
             }
           })
           .done();
